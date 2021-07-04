bdab871e620aac6bb54f6cfaad33163e40f2008f
harrythinhpo <86905554+harrythinhpo@users.noreply.github.com>
2021-07-04 15:31:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ function firstQuestion(){
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
+    var audio = autoplay('sound/Swish1.mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
