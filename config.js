6ea05d37a5635db889f0ed8c1940acbe4c0eea75
harrythinhpo <86905554+harrythinhpo@users.noreply.github.com>
2021-07-04 13:51:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const CONFIG = {
     introTitle: 'Ê con chó!',
     introDesc: `Sài Gòn lạc nhau là mất
     Thế em tính để anh lạc mất em à?`,
-    btnIntro: '',
+    btnIntro: 'Anh Nhớ Em',
     title: 'Trái đất thì cứ xoay và anh thì mãi đắm say vì người',
     desc: 'Có 2 Chuyện muốn nói với e, 1 chuyện ngắn, 1 chuyện dài. ',
     btnYes: 'Chuyện Ngắn <3',
     btnNo: 'Chuyện Dài',
     question:'Chuyện ngắn là anh đang rất nhớ em, Còn chuyện dài gặp nhau a nói cho nghe :)))',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Em xin lỗi',
+    reply: 'Em xin lỗi, E, cũng nhớ Anh :)))',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Khi nào anh có thể gặp em đây',
-    btnAccept: 'NhớNhớ Em Nhiều',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Nhớ Nhớ Em Nhiều',
+    messLink: 'https://www.facebook.com/harrythinhpo42' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
