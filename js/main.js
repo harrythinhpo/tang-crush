2c8f8069c76e14f8a58c2f361968e36a9436f456
harrythinhpo <86905554+harrythinhpo@users.noreply.github.com>
2021-07-04 15:36:41 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,8 +18,7 @@ function init(){
 }
 
 function firstQuestion(){
-   var audio = new Audio('sound/sound.mp3');
-audio.play();
+   var audio = new Audio('sound/sound.mp3' 'autoplay');
     $('.content').hide();
     Swal.fire({
         title: CONFIG.introTitle,
